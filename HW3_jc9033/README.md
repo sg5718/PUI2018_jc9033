d0a90f8e7c967173e0ddba850f68f57e8a9f1112
Junjie Cai <jc9033@ip-10-10-1-15.ec2.internal>
2018-09-23 13:13:31 -0400

    Changes to be committed: 	new file:   HW3_jc9033/.ipynb_checkpoints/Assignment2-checkpoint.ipynb 	new file:   HW3_jc9033/.ipynb_checkpoints/Assignment2_extra-checkpoint.ipynb 	new file:   HW3_jc9033/Assignment2.ipynb 	new file:   HW3_jc9033/Assignment2_extra.ipynb 	new file:   HW3_jc9033/README.md 	new file:   HW3_jc9033/assignment1_image/1.png 	new file:   HW3_jc9033/assignment1_image/2.png 	new file:   HW3_jc9033/assignment1_image/3.png 	deleted:    HW3_jc9033/empty 	new file:   HW3_jc9033/get_bus_info_jc9033.py 	new file:   HW3_jc9033/show_bus_locations_jc9033.py

---
+++ HW3_jc9033/README.md
@@ -0,0 +1,4 @@
+# Assignment 1: delete data and its history from a GitHub repo
+ ![image](HW3_jc9033/assignment1_image/1.png)
+ ![image](HW3_jc9033/assignment1_image/2.png)
+ ![image](HW3_jc9033/assignment1_image/3.png)
---
+++ HW3_jc9033/get_bus_info_jc9033.py
@@ -0,0 +1,42 @@
+import sys
+import os
+import numpy as np
+import pandas as pd
+import csv
+
+url="http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=" + sys.argv[1] + "&VehicleMonitoringDetailLevel=calls&LineRef=" + sys.argv[2]
+BUS = pd.read_json(url)
+try:
+    ActiveBus = BUS['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
+except:
+    print("No Active Bus!")
+    sys.exit(0)
+ActiveBusNum = len(ActiveBus)
+
+print("Bus Line:",sys.argv[2])
+print("Number of Active Buses:",ActiveBusNum)
+print("Latitude ,  Longitude , Stop Name , Stop Status")
+
+csvoutput = ["Latitude" ,  "Longitude" , "Stop Name" , "Stop Status" ]
+with open("busline.csv","w") as csvfile: 
+    writer = csv.writer(csvfile)
+    writer.writerow(csvoutput)
+    csvfile.close()
+    
+for i in range(ActiveBusNum):
+    location = ActiveBus[i]['MonitoredVehicleJourney']['VehicleLocation']
+    try:
+        StopName = ActiveBus[i]['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][1]['StopPointName']
+        StopStatus = ActiveBus[i]['MonitoredVehicleJourney']['MonitoredCall']['Extensions']['Distances']['PresentableDistance']
+    except:
+        StopStatus = "N/A"
+        StopName = "N/A"
+    print("%.6f" % location['Latitude'] ,",", "%.6f" % location['Longitude'] ,",", StopName ,",", StopStatus)      
+    csvoutput=(["%.6f" % location['Latitude'] , "%.6f" % location['Longitude'] , StopName , StopStatus])
+    with open("busline.csv","a") as csvfile: 
+        writer = csv.writer(csvfile)
+        writer.writerow(csvoutput)
+        csvfile.close()   
+
+newname = sys.argv[2] + '.csv'
+os.rename("busline.csv", newname)
